2a870571a3c2614a7d9bce391798b79abe168dda
Travis Szucs <travisszucs@me.com>
2017-03-13 20:52:59 -0700

    Added pretty font!

--- sys/main.cpp
+++ sys/main.cpp
@@ -8,8 +8,25 @@
 #include "BearLibTerminal.h"
 #include "../havenlib/precompiled.h"
 
+/* Conditional for main game loop */
+static bool runGame = true;
+
 int main() {
-    terminal_open();
+    /* Create our main window */
+    if ( !terminal_open() ) {
+        DebugPrintf( VERBOSITY_SYS, "Failed to open terminal window.. exiting\n" );
+        return -1;
+    }
+
+    /* Terminal settings */
+    if ( !terminal_set( "window: title='AVA', size=200x50" ) ) {
+        DebugPrintf( VERBOSITY_SYS, "Failed to set window title and size... exiting\n" );
+        return -1;
+    }
+    if ( !terminal_set( "font: ./NotoMono-Regular.ttf, size=11" ) ) {
+        DebugPrintf( VERBOSITY_SYS, "Failed to set terminal font.. exiting\n" );
+        return -1;
+    }
 
     terminal_print( 0, 0, "Oh yeah!" );
     terminal_print( 0, 1, "You gotta get schwifty" );
@@ -18,7 +35,17 @@ int main() {
     terminal_refresh();
 
     DebugPrintf( VERBOSITY_SYS, "Waiting for terminal to be closed..\n" );
-    while ( terminal_read() != TK_CLOSE );
+    while ( runGame ) {
+        switch ( terminal_read() ) {
+        case TK_CLOSE:
+            runGame = false;
+            break;
+        default:
+            break;
+        }
+    }
 
     terminal_close();
+
+    return 0;
 }
