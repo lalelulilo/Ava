6c929c7f44dfe749a122761e3700bd752c385fff
Vincent.Tennant <vincent.tennant@gmail.com>
2017-03-13 22:02:54 -0700

    Added simple border inside of a non-resizeable window. Basic collision next.

--- sys/main.cpp
+++ sys/main.cpp
@@ -8,17 +8,59 @@
 #include "BearLibTerminal.h"
 #include "../havenlib/precompiled.h"
 
+const char* terminal_params = "window: title='Ava', size=80x25";
+
+//TODO (tennent)Move this outside main.
+/*
+=================
+map
+
+	(tennent) For now this will border the window with "walls".
+=================
+*/
+int map() {
+	int width;
+	int height;
+
+	//(tennant) probably shouldn't be done here.
+	terminal_set(terminal_params); 
+
+	terminal_clear();
+
+	//(tennent) Setting "envioronment" layer to 1 for the tiem being.
+	terminal_layer(1);
+
+	width = terminal_state(TK_WIDTH);
+	height = terminal_state(TK_HEIGHT);
+
+	//(tennant) bordering current window as the first "room". Can these loops be combined?
+	//(tennent) for top and bottom
+	for (int x = 2; x<width-2; x++)
+	{
+		terminal_put(x, 2, (int)'#');
+		terminal_put(x, height - 3, (int)'#');
+	}
+
+	//(tennant) for left and right side
+	for (int y = 2; y<height-2; y++)
+	{
+		terminal_put(2, y, (int)'#');
+		terminal_put(width - 3, y, (int)'#');
+	}
+
+	terminal_refresh();
+
+	while (terminal_read() != TK_CLOSE);
+
+	return 0;
+}
+
 int main() {
-    terminal_open();
+	terminal_open();
 
-    terminal_print( 0, 0, "Oh yeah!" );
-    terminal_print( 0, 1, "You gotta get schwifty" );
-    terminal_print( 0, 2, "You gotta get schwifty in here" );
-    terminal_print( 0, 3, "It's time to get schwifty" );
-    terminal_refresh();
+	while (map());
 
-    DebugPrintf( VERBOSITY_SYS, "Waiting for terminal to be closed..\n" );
-    while ( terminal_read() != TK_CLOSE );
+	terminal_close();
 
-    terminal_close();
+	DebugPrintf(VERBOSITY_SYS, "Waiting for terminal to be closed..\n");
 }
