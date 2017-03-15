58421c06246c077212586f93d7ede64ae9035157
Vincent.Tennant <vincent.tennant@gmail.com>
2017-03-14 22:09:19 -0700

    Cleaned up.

--- sys/main.cpp
+++ sys/main.cpp
@@ -1,24 +1,32 @@
-/* Entry point for AVA */
+/*
+=================
+main.cpp
 
-/* Note(tszucs): This allows us to start from main() instead of WinMain().. */
+TODO (tennent) Proper class documentation will be requierd. As we flesh out the perpose of each class we can 
+			   update the documentation.
+
+The main entry point for AVA. This class will include the main game loop (among other things).
+=================
+*/
+
+/* (tszucs): This allows us to start from main() instead of WinMain().. */
 #ifdef _MSC_VER
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
-
-#include <vector>
 
 #include "BearLibTerminal.h"
 #include "../havenlib/precompiled.h"
 
 const char* terminal_params = "window: title='Ava', size=80x25"; //(tennent) size is WIDTHxHEIGHT
-const int width = TK_WIDTH;
 
 //TODO (tennent)Move this outside main.
 /*
 =================
 map
 
-(tennent) Generates a single array with "walls". The array will (could be) used for collision and other environmental stuff.
+	(tennent) Generates a single array with "walls". The array will (could be) used for collision and other 
+	environmental stuff. This was a tricky due to BearLib reversing the traditional ROW vs COL 
+	to COL vs ROW for its terminals grid.
 =================
 */
 template <size_t cols, size_t rows>
@@ -47,11 +55,7 @@ map
 	(tennent) For now this will border the window with "walls".
 =================
 */
-int map() {
-
-	//(tennant) probably shouldn't be done here.
-	terminal_set(terminal_params); 
-
+void map() {
 	//(tennant) predetermined window sizes for now. Can figure out dynamic changes later. Arrays probably aren't the best soln here.
 	const int width = 80;
 	const int height = 25;
@@ -63,9 +67,6 @@ int map() {
 			simple_map[i][j] = ' ';
 		}
 	}
-	
-
-	terminal_clear();
 
 	//(tennent) Setting "envioronment" layer to 1 for the time being.
 	terminal_layer(1);
@@ -73,27 +74,40 @@ int map() {
 	//generate a simple map
 	map_gen(simple_map);
 
-	//(tennent) Iterate through array and place the characters.
+	//(tennent) Iterate through array and place the characters on screen.
 	for (int i = 0; i < height; i++) {
 		for (int j = 0; j < width; j++) {
 			if (simple_map[i][j] == '#')
 				terminal_put(j, i, simple_map[i][j]);
 		}
 	}
-
-	terminal_refresh();
-
-	while (terminal_read() != TK_CLOSE);
-
-	return 0;
 }
 
 int main() {
+
+	//(tennant) add debug info
 	terminal_open();
 
-	while (map());
+	//(tennant) add debug info
+	//(tennent) set terminal parameters.
+	terminal_set(terminal_params);
 
+	//(tennant) add debug info
+	terminal_clear();
+
+	//(tennant) draw the map in the terminal.
+	map();
+
+	//(tennant) add debug info
+	terminal_refresh();
+
+	//(tennent) wait for close.
+	while (terminal_read() != TK_CLOSE);
+
+	//(tennant) add debug info
 	terminal_close();
 
 	DebugPrintf(VERBOSITY_SYS, "Waiting for terminal to be closed..\n");
+
+	return 0;
 }
